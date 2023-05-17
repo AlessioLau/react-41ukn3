3a77aff616fb4a6f7fb289b1d0f3c5a2656445ad
AlessioLau <102764016+AlessioLau@users.noreply.github.com>
2023-05-17 17:43:11 -0300

    3 files modified

--- src/App.js
+++ src/App.js
@@ -10,3 +10,32 @@ export default function App() {
     </div>
   );
 }
+
+function App() {
+  return (
+    <h1>Bienvenidos a Desarrollo de Software!</h1>
+    );
+}
+export default App;
+
+function App() {
+  return (
+     <div className="mt-4 p-5 rounded" style={{backgroundColor:"lightgray"}} >
+        <h1>Pymes 2023</h1>
+        <p>Este ejemplo está desarrollado con las siguientes tecnologías:</p>
+        <p>
+          Backend: NodeJs, Express , WebApiRest, Swagger, Sequelize, Sqlite y 
+          múltiples capas en Javascript.
+        </p>
+        <p>
+          Frontend: Single Page Application, HTML, CSS, Bootstrap, NodeJs,
+          Javascript y React.
+        </p>
+        <button className="btn btn-lg btn-primary">
+          <i className="fa fa-search"> </i>
+          Ver Articulos Familias
+        </button>
+      </div>
+  );
+}
+export default App;
